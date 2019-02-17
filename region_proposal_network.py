c0760382f5a3045e93f4752c5cd5fbacd07e49c4
D0CX4ND3R <doc.x.washington@gmail.com>
2019-02-18 07:57:56 +0800

    Up RPN codes

--- region_proposal_network.py
+++ region_proposal_network.py
@@ -76,10 +76,75 @@ def rpn(feature_map, rpn_config=RpnConfig(2)):
         # rpn_cls_score
         rpn_cls_score = slim.conv2d(features, 2 * rpn_config.anchor_num, [1, 1],
                                     activation_fn=None, scope='rpn_cls_score')
+        rpn_cls_score = tf.reshape(rpn_cls_score, [-1, 2])
+        rpn_cls_pred = slim.softmax(rpn_cls_score, scope='rpn_cls_pred')
 
-        # rpn_cls_prob
-        rpn_cls_prob = slim.conv2d(rpn_cls_score, rpn_config.anchor_num * rpn_config.class_count, [1, 1],
-                                   activation_fn=None, scope='rpn_cls_prob')
+        # rpn_bbox_pred
+        rpn_bbox_pred = slim.conv2d(features, rpn_config.anchor_num * rpn_config.class_count, [1, 1],
+                                    activation_fn=None, scope='rpn_bbox_pred')
+        rpn_bbox_pred = tf.reshape(rpn_bbox_pred, [-1, 4])
+
+    return rpn_cls_pred, rpn_bbox_pred
+
+
+def process_rpn_proposals(anchors, rpn_cls_pred, rpn_bbox_pred, image_shape, scale_factor=None):
+    # 1. Trans bboxes
+    t_xcenters, t_ycenters, t_w, t_h = tf.unstack(rpn_bbox_pred, axis=1)
+
+    if scale_factor:
+        t_xcenters /= scale_factor[0]
+        t_ycenters /= scale_factor[1]
+        t_w /= scale_factor[2]
+        t_h /= scale_factor[3]
+
+    anchors_x_min, anchors_y_min, anchors_x_max, anchors_y_max = tf.unstack(anchors, axis=1)
+    anchors_width = anchors_x_max - anchors_x_min
+    anchors_height = anchors_y_max - anchors_y_min
+    anchors_center_x = anchors_x_min + anchors_width / 2.0
+    anchors_center_y = anchors_y_min + anchors_height / 2.0
+
+    # According to equations in Faster-RCNN
+    # t_x = (x - x_a) / w_a
+    # t_y = (y - y_a) / h_a
+    # t_w = log(w / w_a)
+    # t_h = log(h / h_a)
+    # Where (t_x, t_y, t_w, t_h) is the prediction by RPN, (x, y, w, h) is the prediction of bounding box,
+    # (x_a, y_a, w_a, h_a) is the generated anchor box.
+    # The RPN will be optimized to calculate the coordinates of (t_x, t_y, t_w, t_h).
+    predict_center_x = t_xcenters * anchors_width + anchors_center_x
+    predict_center_y = t_ycenters * anchors_height + anchors_center_y
+    predict_width = tf.exp(t_w) * anchors_width
+    predict_height = tf.exp(t_h) * anchors_height
+
+    predict_x_min = predict_center_x - predict_width / 2
+    predict_y_min = predict_center_y - predict_height / 2
+    predict_x_max = predict_center_x + predict_width / 2
+    predict_y_max = predict_center_y + predict_height / 2
+
+    # 2. Clip bounding boxes, make all boxes in the bounding of image
+    image_height, image_width = image_shape
+    predict_x_min = tf.maximum(0, tf.minimum(image_width-1, predict_x_min))
+    predict_y_min = tf.maximum(0, tf.minimum(image_height-1, predict_y_min))
+
+    predict_x_max = tf.maximum(0, tf.minimum(image_width-1, predict_x_max))
+    predict_y_max = tf.maximum(0, tf.minimum(image_height-1, predict_y_max))
+
+    predict_bboxes = tf.transpose(tf.stack([predict_x_min, predict_y_min, predict_x_max, predict_y_max]))
+
+    predict_targets_count = tf.minimum(12000, tf.shape(predict_bboxes)[0])
+    sorted_cls_scores, sorted_pred_indeces = tf.nn.top_k(rpn_cls_pred, predict_targets_count)
+    sorted_bounding_boxes = tf.gather(predict_bboxes, sorted_pred_indeces)
+    selected_bboxes_indeces = tf.image.non_max_suppression(sorted_bounding_boxes, sorted_cls_scores,
+                                                           image_shape[0] * image_shape[1])
+
+    selected_bboxes = tf.gather(sorted_bounding_boxes, selected_bboxes_indeces)
+    selected_scores = tf.gather(sorted_cls_scores, selected_bboxes_indeces)
+    return selected_bboxes, selected_scores
+
+
+def build_rpn_loss(rois, roi_scores):
+    preprocess_image_batch =
+
 
 if __name__ == '__main__':
     a = generate_anchors([0, 0, 15, 15])
